2981965b94fc70822c27c48125be41d1d27e3f46
Vincent <Yip.vincent@gmail.com>
2023-03-01 00:38:56 -0500

    update production url whitelist

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ app.set("trust proxy", 1);
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://gridstore-backend.herokuapp.com/"
+  );
 
   res.setHeader(
     "Access-Control-Allow-Headers",
