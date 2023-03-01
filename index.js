7892841282f5e2ce26d02ae99df4f306327cf66c
Vincent <Yip.vincent@gmail.com>
2023-03-01 00:51:54 -0500

    fix url for whitelist

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://gridstore-backend.herokuapp.com/"
+    "https://gridstore-backend.herokuapp.com"
   );
 
   res.setHeader(
