9adc7e240177033510b714346ffcd94defe7241a
Vincent <Yip.vincent@gmail.com>
2023-03-01 15:14:16 -0500

    add cors access to project production url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://gridstore-backend.herokuapp.com"
+    "https://gridstore-d785.vercel.app/"
   );
 
   res.setHeader(
